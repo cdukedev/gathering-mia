1f705c6f5f74f7e7cd6c727b075a66f431464076
corey duke <cdukedev@gmail.com>
2022-08-26 20:12:35 -0400

    added a desktop component to display when screen is too big

--- client/src/pages/Home/HomeComponents/Desktop/Desktop.js
+++ client/src/pages/Home/HomeComponents/Desktop/Desktop.js
@@ -1,22 +1,30 @@
 import Logo from "../../../../assets/logo/desktop-logo.png";
 import QRCode from "../../../../assets/icons/desktop-qr.png";
+import "./Desktop.scss";
 
 function Desktop() {
   return (
     <div className="desktop">
-      <div className="desktop-logo">
-        <img src={Logo} alt="logo" />
+      <div className="desktop-logo__container">
+        <img className="desktop-logo" src={Logo} alt="logo" />
       </div>
-      <p className="desktop-content">
-        We are excited to have to join us! Please scan the QR code to navigate
-        to our site on your mobile device
-      </p>
-      <div className="desktop-qr">
-        <img src={QRCode} alt="gathering website QR Code" />
+      <div className="desktop-content__container">
+        <p className="desktop-content">
+          We are excited to have to join us! This site is is intented for mobile
+          only. Please scan the QR code to navigate to our site on your mobile
+          device
+        </p>
+        <div>
+          <img
+            className="desktop-qr"
+            src={QRCode}
+            alt="gathering website QR Code"
+          />
+        </div>
+        <span className="desktop-content">
+          Or input gathering-mia.live into your favorite mobile browser.
+        </span>
       </div>
-      <span>
-        Or input gathering-mia.live into your favorite mobile browser.
-      </span>
     </div>
   );
 }
