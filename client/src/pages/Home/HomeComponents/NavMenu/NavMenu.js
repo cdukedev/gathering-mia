c74ac3108f096b02aa850c4ca2252ae41ac73fbe
corey duke <cdukedev@gmail.com>
2023-04-16 12:11:15 -0400

    fixed scroll issue

--- client/src/pages/Home/HomeComponents/NavMenu/NavMenu.js
+++ client/src/pages/Home/HomeComponents/NavMenu/NavMenu.js
@@ -1,14 +1,20 @@
 import "./NavMenu.scss";
 import NavButton from "../../../../assets/icons/nav-button.svg";
 import { Link } from "react-router-dom";
+import { animateScroll as scroll } from "react-scroll";
 
-function NavMenu(props) {
-  if (!props.navMenu) {
+function NavMenu({ handleNavMenu, navMenu }) {
+  const scrollToElement = (hash) => {
+    scroll.scrollTo(document.getElementById(hash).offsetTop);
+    handleNavMenu();
+  };
+
+  if (!navMenu) {
     return (
       <div className="nav-menu nav-menu-off">
         <div className="nav-menu__button-container">
           <img
-            onClick={props.handleNavMenu}
+            onClick={handleNavMenu}
             className="nav-menu__button"
             src={NavButton}
             alt="nav-button"
@@ -21,10 +27,7 @@ function NavMenu(props) {
       <>
         <div className="nav">
           <div className="nav-menu">
-            <div
-              className="nav-menu__button-container"
-              onClick={props.handleNavMenu}
-            >
+            <div className="nav-menu__button-container" onClick={handleNavMenu}>
               <img
                 className="nav-menu__button"
                 src={NavButton}
@@ -40,33 +43,25 @@ function NavMenu(props) {
               </Link>
               <Link
                 className="nav-menu__item"
-                smooth
-                to="/#AboutUs"
-                onClick={props.handleNavMenu}
+                onClick={() => scrollToElement("AboutUs")}
               >
                 ABOUT US
               </Link>
               <Link
                 className="nav-menu__item"
-                smooth
-                to="/#TakePart"
-                onClick={props.handleNavMenu}
+                onClick={() => scrollToElement("TakePart")}
               >
                 GET INVOLVED
               </Link>
               <Link
                 className="nav-menu__item"
-                smooth
-                to="/#NeedHelp"
-                onClick={props.handleNavMenu}
+                onClick={() => scrollToElement("NeedHelp")}
               >
                 NEED FOOD ASSISTANCE?
               </Link>
               <Link
                 className="nav-menu__item"
-                smooth
-                to="/#contact"
-                onClick={props.handleNavMenu}
+                onClick={() => scrollToElement("contact")}
               >
                 CONTACT US
               </Link>
@@ -77,4 +72,5 @@ function NavMenu(props) {
     );
   }
 }
+
 export default NavMenu;
