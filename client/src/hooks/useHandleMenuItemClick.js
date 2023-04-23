104741035c17c339601a1d101b094beadef7eb83
corey duke <cdukedev@gmail.com>
2023-04-22 20:23:34 -0400

    Added util for menu item click

---
+++ client/src/hooks/useHandleMenuItemClick.js
@@ -0,0 +1,14 @@
+import { useCallback } from "react";
+import { scrollToElement } from "../utils/scrollToElement";
+
+export const useHandleMenuItemClick = (handleNavMenu) => {
+  const handleMenuItemClick = useCallback(
+    (hash) => {
+      scrollToElement(hash);
+      handleNavMenu();
+    },
+    [handleNavMenu]
+  );
+
+  return handleMenuItemClick;
+};
