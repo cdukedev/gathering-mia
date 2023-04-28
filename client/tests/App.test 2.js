d9671336c975ca4ddbf4038031b44020081a443b
corey duke <cdukedev@gmail.com>
2023-04-28 13:36:03 -0400

    added simple render test for App.js

---
+++ client/tests/App.test 2.js
@@ -0,0 +1,8 @@
+import React from "react";
+import App from "../src/App";
+import renderer from "react-test-renderer";
+
+it("renders App component correctly", () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
