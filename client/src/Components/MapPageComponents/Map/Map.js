7e1adde8a45cd449a1a2852b31b1fca0f3927018
corey duke <cdukedev@gmail.com>
2023-04-29 16:11:25 -0400

    Adjusted API Variable name

--- client/src/Components/MapPageComponents/Map/Map.js
+++ client/src/Components/MapPageComponents/Map/Map.js
@@ -44,7 +44,7 @@ const Map = (props) => {
   };
 
   return (
-    <LoadScript googleMapsApiKey={process.env.REACT_APP_API_KEY}>
+    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY}>
       {center && (
         <GoogleMap
           mapContainerStyle={containerStyle}
