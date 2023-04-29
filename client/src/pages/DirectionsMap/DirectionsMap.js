c4e14f39a627f6af89d211d8d6f2bcc3f602a1ec
corey duke <cdukedev@gmail.com>
2023-04-29 16:03:26 -0400

    Have update the marker, deploying to test in the car. Voice needs to be fixed reading all direction from start

--- client/src/pages/DirectionsMap/DirectionsMap.js
+++ client/src/pages/DirectionsMap/DirectionsMap.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import {
   GoogleMap,
@@ -8,9 +8,24 @@ import {
 
 const DirectionsMap = () => {
   const { userLat, userLng, foodBankLat, foodBankLng } = useParams();
+  const center = {
+    lat: parseFloat(userLat),
+    lng: parseFloat(userLng),
+  };
   const [directions, setDirections] = useState(null);
   const [error, setError] = useState(null);
-  
+  const [userPosition, setUserPosition] = useState(center);
+  const [carMarker, setCarMarker] = useState(null);
+  const [watchId, setWatchId] = useState(null);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    return () => {
+      if (watchId) {
+        navigator.geolocation.clearWatch(watchId);
+      }
+    };
+  }, [watchId]);
 
   const containerStyle = {
     width: "100%",
@@ -26,21 +41,47 @@ const DirectionsMap = () => {
     fullscreenControl: false,
   };
 
-  const center = {
-    lat: parseFloat(userLat),
-    lng: parseFloat(userLng),
+  const updateCarMarker = (position) => {
+    if (carMarker) {
+      carMarker.setPosition(position);
+    } else {
+      const newMarker = new window.google.maps.Marker({
+        position,
+        map: mapRef.current,
+        icon: "https://maps.gstatic.com/mapfiles/ms2/micons/red-pushpin.png",
+      });
+      setCarMarker(newMarker);
+    }
+    setUserPosition(position);
+  };
+  const handleGeolocationUpdate = (position) => {
+    const newPosition = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+    };
+    updateCarMarker(newPosition);
+  };
+  const speakDirections = (text) => {
+    const synth = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance(text);
+    synth.speak(utterance);
   };
 
   const directionsCallback = (response, status) => {
     if (status === "OK") {
       setDirections(response);
+      const steps = response.routes[0].legs[0].steps;
+      steps.forEach((step) => {
+        speakDirections(step.instructions);
+      });
     } else {
       console.error(`Error fetching directions: ${status}`);
       setError(`Error fetching directions: ${status}`);
     }
   };
 
-  const handleMapLoad = () => {
+  const handleMapLoad = (map) => {
+    mapRef.current = map;
     if (!directions) {
       const request = {
         origin: center,
@@ -54,6 +95,15 @@ const DirectionsMap = () => {
         request,
         directionsCallback
       );
+    }
+
+    if (!watchId) {
+      const id = navigator.geolocation.watchPosition(
+        handleGeolocationUpdate,
+        (error) => console.error(error),
+        { enableHighAccuracy: true, maximumAge: 0, timeout: 5000 }
+      );
+      setWatchId(id);
     }
   };
 
