956f0babced120ceee7fb1e6c1a59fbf23b4d79a
corey duke <cdukedev@gmail.com>
2023-05-09 16:10:34 -0400

    Added components to the app.js for better routing and linking.

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MapPage from "./pages/MapPage/MapPage";
 import Home from "./pages/Home/Home";
 import DirectionsMap from "./pages/DirectionsMap/DirectionsMap";
+import QRScanner from "./Components/MapPageComponents/MapDeliveryComponents/QRScanner/QRScanner";
 import { FoodBankProvider } from "./contexts/FoodBankContext";
 import { CommunityGardenProvider } from "./contexts/CommunityGardenContext";
 import { RecipientProvider } from "./contexts/RecipientContext";
 import { GeolocationProvider } from "./contexts/GeolocationContext";
 import { MapPageProvider } from "./contexts/MapPageContext";
+import Deliveries from "./Components/MapPageComponents/MapDeliveryComponents/Deliveries/Deliveries";
 
 function App() {
   return (
@@ -35,6 +37,23 @@ function App() {
           path="/directions/:userLat/:userLng/:foodBankLat/:foodBankLng"
           element={<DirectionsMap />}
         />
+        <Route path="/deliver" element={<QRScanner />}></Route>
+        <Route
+          path="/deliveries"
+          element={
+            <FoodBankProvider>
+              <CommunityGardenProvider>
+                <RecipientProvider>
+                  <GeolocationProvider>
+                    <MapPageProvider>
+                      <Deliveries />
+                    </MapPageProvider>
+                  </GeolocationProvider>
+                </RecipientProvider>
+              </CommunityGardenProvider>
+            </FoodBankProvider>
+          }
+        ></Route>
       </Routes>
     </Router>
   );
